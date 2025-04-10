0a122d6a8d26db5604febc22c11b438da824de96
H Movaghari <hmovaghari@gmail.com>
2025-04-10 11:11:11 +0330

    Version 1.0.4

--- DNSChanger/DNSChanger/Properties/Resources.Designer.cs
+++ DNSChanger/DNSChanger/Properties/Resources.Designer.cs
@@ -279,7 +279,7 @@ namespace DNSChanger.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 1.0.3.
+        ///   Looks up a localized string similar to 1.0.4.
         /// </summary>
         internal static string Version {
             get {
